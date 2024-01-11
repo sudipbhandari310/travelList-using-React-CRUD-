24c1c03df050fd5ede4f58c59b3c44fec437f36e
Sudip <sudipbhandari310@gmail.com>
2024-01-11 21:02:05 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return (
+    <div>
+      <h1>Bag Packers 🏝 </h1>
+    </div>
+  );
+}
+function Form() {
+  return (
+    <div>
+      <h3 className="add-form">What do you need for your trip ? 👜🎒</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span>
+        {item.quantity}
+        {item.description}
+      </span>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <em>
+      <footer className="stats">
+        You have x items in your list and you have packed y items.
+      </footer>
+    </em>
+  );
+}
